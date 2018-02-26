fd6f60af44417f629389a9d0b5a86b82b87628a2
AntTAKEOVER <vedantpatwari01@gmail.com>
2018-02-26 11:01:12 +0530

    Step

--- Initial.py
+++ Initial.py
@@ -1,12 +1,15 @@
-counter = 1 
-step_size = 5
+try:
+    f = open("test.srt")
+    for line in f: 
+        print(line, end = " ")
+   
+    f.seek(0) 
+    
+    
+finally:
+    
+    f.close()
+    
 
-json = 0 # json returned by watson 
-
-indid # sentence 
-  
-subject = "<font color='#6d9eeb'>"
-objec = "<font color='#8e7cc3'>"
-verb = "<font color='#93c74d'>"
 
  
