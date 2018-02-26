bd8bdeeca8d910ee8acfb4728be83736d4623367
AntTAKEOVER <vedantpatwari01@gmail.com>
2018-02-26 10:41:01 +0530

    Generating text from mp4 files

---
+++ Initial.py
@@ -0,0 +1,12 @@
+counter = 1 
+step_size = 5
+
+json = 0 # json returned by watson 
+
+indid # sentence 
+  
+subject = "<font color='#6d9eeb'>"
+objec = "<font color='#8e7cc3'>"
+verb = "<font color='#93c74d'>"
+
+ 
